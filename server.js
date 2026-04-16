ea3de6f82c2c6978d15d055c8ca6f4c5f7118d40
younaswazir <younaskhanwaz80@gmail.com>
2026-04-16 23:37:42 +0500

    change port variable setting

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use((err, req, res, next) => {
 })
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server is listening to port: ${PORT}`);
