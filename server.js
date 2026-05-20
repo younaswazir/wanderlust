4682af1fd5d49b07a14176284cd77faab02216fb
younaswazir <younaskhanwaz80@gmail.com>
2026-05-20 23:58:42 +0500

    added built command for deployment

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ connectDB();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate)
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride("_method"));
